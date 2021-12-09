3cebadf1ff5fdcdd1da4cca88c949c646340b00e
Christopher Akoto <71773056+spartamonk@users.noreply.github.com>
2021-12-09 07:17:44 -0600

    products display resolved

--- examples/3-airtable/app.js
+++ examples/3-airtable/app.js
@@ -1,1 +1,26 @@
 const result = document.querySelector('.result')
+
+const fetch =async() => {
+ try {
+const {data} = await axios.get('/api/3-airtable');
+const products= data.map(i=> {
+ const {id, name, url, price} = i;
+ return ` <article class="product">
+      <img
+        src=${url}
+        alt=${name}
+      />
+      <div class="info">
+        <h5>${name}</h5>
+        <h5 class="price">$${name}</h5>
+      </div>
+    </article>`
+}).join('');
+result.innerHTML = products
+ }
+ catch(error) {
+console.log(error.response);
+ }
+}
+
+fetch()
