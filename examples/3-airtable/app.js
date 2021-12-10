07a28aa781a21e1185d9e2c434f8dfdd2ee02048
Christopher Akoto <71773056+spartamonk@users.noreply.github.com>
2021-12-09 23:23:47 -0600

    products fetch updated

--- examples/3-airtable/app.js
+++ examples/3-airtable/app.js
@@ -2,7 +2,8 @@ const result = document.querySelector('.result')
 
 const fetch =async() => {
  try {
-const {data} = await axios.get('/api/3-airtable');
+// const {data} = await axios.get('/api/3-airtable');
+const { data } = await axios.get('/api/3-z-complete')
 const products= data.map(i=> {
  const {id, name, url, price} = i;
  return ` <a href='./product?id=${id}' class="product">
